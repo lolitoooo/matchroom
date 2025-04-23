c9cac7bf6659a27ef01fe130230d45f63d1ef300
lolitoooo <loanpena77@gmail.com>
2025-04-23 02:32:30 +0200

    Fonctionnalité: Système d'authentification avec formulaire de connexion personnalisé

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Si user connecté, rediriger vers la page d'accueil
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        // Récupérer erreur de connexion si elle existe
+        $error = $authenticationUtils->getLastAuthenticationError();
+        
+        // Récupérer le dernier nom d'utilisateur saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+        
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('Cette méthode peut être vide - elle sera interceptée par la clé de déconnexion de votre pare-feu.');
+    }
+}
