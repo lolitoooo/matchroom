c20b8da359abcccbade5c707d39aea49bc6e0b9e
lolitoooo <loanpena77@gmail.com>
2025-04-23 02:40:11 +0200

    Fonctionnalité: Carte interactive avec Leaflet pour afficher les hôtels

---
+++ src/Controller/MapController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Hotel;
+use App\Repository\HotelRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MapController extends AbstractController
+{
+    #[Route('/map', name: 'app_map')]
+    public function index(HotelRepository $hotelRepository): Response
+    {
+        return $this->render('map/index.html.twig', [
+            'controller_name' => 'MapController',
+        ]);
+    }
+
+    #[Route('/map/hotels', name: 'app_map_hotels_data', methods: ['GET'])]
+    public function getHotelsData(HotelRepository $hotelRepository): JsonResponse
+    {
+        $hotels = $hotelRepository->findAll();
+        
+        $hotelsData = [];
+        foreach ($hotels as $hotel) {
+            if ($hotel->getLatitude() && $hotel->getLongitude()) {
+                $hotelsData[] = [
+                    'id' => $hotel->getId(),
+                    'name' => $hotel->getName(),
+                    'address' => $hotel->getAddress(),
+                    'description' => $hotel->getDescription(),
+                    'latitude' => $hotel->getLatitude(),
+                    'longitude' => $hotel->getLongitude(),
+                ];
+            }
+        }
+        
+        return new JsonResponse($hotelsData);
+    }
+}
